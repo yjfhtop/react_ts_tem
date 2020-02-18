7cd485d12cca8b4bb6b69e52aa660e52c9783f11
yang <1762086020@qq.com>
2020-02-18 20:38:02 +0800

    update: 配置转发

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+// 这里属于配置代理  建议在这里进行配置， 而不是在json 文件
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(proxy(process.env.REACT_APP_BASE_API, {target: process.env.REACT_APP_BASE_API_TO_PATH}));
+};
