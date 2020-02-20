d9235bc7d7e7ad8c678265d47dae92fe00c25efe
yang <1762086020@qq.com>
2020-02-20 20:38:07 +0800

    update: 用户登录之后，将所有路由添加至 权限路由

--- src/routes/base.tsx
+++ src/routes/base.tsx
@@ -5,7 +5,7 @@ import baseHOC from '@/hoc/base'
 import permissionHOC from '@/hoc/permission'
 
 const home = lazy(() => import("../view/home/home"));
-const login = lazy(() => import("../view/login/login"));
+const login = lazy(() => import("../view/account/login/login"));
 const notFound = lazy(() => import("../view/errPage/404/404"));
 
 const routers: RouteConfig[] = [
@@ -13,7 +13,6 @@ const routers: RouteConfig[] = [
         key: 'home',
         path: '/',
         exact: true,
-        // component: baseRouterHOC(suspenseComponentHOC(home)),
         component: permissionHOC(baseHOC(home)),
         meta: {
             title: '首页'
@@ -24,7 +23,6 @@ const routers: RouteConfig[] = [
         path: '/login',
         exact: true,
         component: permissionHOC(baseHOC(login)),
-        // component: login,
         meta: {
             title: '首页'
         }
@@ -32,7 +30,7 @@ const routers: RouteConfig[] = [
     {
         key: '404',
         path: '/404',
-        component: notFound,
+        component: baseHOC(notFound),
         meta: {
             title: '404'
         }
--- src/stores/user/actions.ts
+++ src/stores/user/actions.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux';
 import constants from './constants'
 import { userLogin, getUserInfo } from '@/api/user'
 import { updatePermissionRouteAct } from '../permission/actions'
+import allRouter from '@/routes'
 
 
 export function updateUserInfoAct<T = StoreType.UserInfo>(userInfo: T): StoreType.Action<T> {
@@ -40,7 +41,27 @@ export function getUserInfoAct(reqConfig?: ApiType.ReqConfig) {
         return new Promise((resolve, reject) => {
             getUserInfo(reqConfig).then(data => {
                 dispatch(updateUserInfoAct(data.data.userInfo))
-                const routes = data.data.routers ? data.data.routers : {}
+
+                // 暂时不使用后台 权限 s
+                /*const routes = data.data.routers ? data.data.routers : {}*/
+                // 暂时不使用后台 权限 e
+
+                // 获取用户信息后 将所有路由添加至权限路由 暂时用法 s
+                const routes: { [key: string]: StoreType.PermissionRoute } = {}
+                allRouter.forEach(item => {
+                    if (item.key) {
+                        /*let citem: StoreType.PermissionRoute = {
+                            'key': item.key!,
+                            'path': item.path
+                        }*/
+                        routes[item.key] = {
+                            key: item.key as string,
+                            path: item.path as string
+                        }
+                    }
+                })
+                // 获取用户信息后 将所有路由添加至权限路由 暂时用法 e
+
                 dispatch(updatePermissionRouteAct(routes))
                 resolve(data)
             }).catch(err => {
